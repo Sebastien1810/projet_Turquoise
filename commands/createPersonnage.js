b301af9cde38c8d7615c0b0014dc52aed25f6a19
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-04-04 03:08:32 +0200

    create a personnage and the index.js file

---
+++ commands/createPersonnage.js
@@ -0,0 +1,13 @@
+const createPersonnage = require("../commands/createPersonnage");
+
+module.exports = {
+  name: "createPersonnage",
+  execute(message, args) {
+    if (args[0] === undefined) {
+      message.reply("tu dois entrer un nom! Exemple: !createPersonnage Lucas");
+    } else {
+      const nom = args[0];
+      message.reply(`Un nouveau personnage ${nom} a été invoqué`);
+    }
+  },
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+require(`dotenv`).config();
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const token = process.env.DISCORD_TOKEN; //j'ai utiliser un.env donc je le recup depuis process.env
+const fs = require("fs");
+const path = require("path");
+const commands = new Map();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildMessages, //permets l'acces géneral au serveur
+    GatewayIntentBits.MessageContent, // recevoir les messages
+    GatewayIntentBits.Guilds, // lire le texte des messages
+  ],
+}); //Creer une nouvelle instance
+// (Les intents sont des autorisations que je donne au bot pour lui dire "voici les types d’événements que je veux recevoir depuis Discord)
+const commandFiles = fs.readdirSync(path.join(__dirname, "commands"));
+// ↑ __dirname = dossier actuel (là où se trouve index.js)
+// ↑ path.join(...) = construit un chemin complet vers le dossier "commands"
+// ↑ fs.readdirSync(...) = lit tous les fichiers de ce dossier (ex: ["createPersonnage.js", "stats.js"])
+
+for (const file of commandFiles) {
+  const filePath = path.join(__dirname, "commands", file);
+  //  Importer le fichier commande (chaque fichier doit exporter { name, execute })
+  const command = require(filePath);
+  commands.set(command.name, command.execute);
+}
+//Quand le client est prêt, run le code (uniquement une fois)
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready!Logged in as ${readyClient.user.tag}`);
+});
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return; // On ignore les messages des bots
+  if (!message.content.startsWith("!")) return; // On ignore les messages sans "!"
+
+  const args = message.content.slice(1).trim().split(/ +/); // ["createPersonnage", "Lucas"]
+  const commandName = args.shift().toLowerCase(); // "createPersonnage"
+  const command = commands.get(commandName);
+
+  if (command) {
+    command(message, args); // Exécute la commande
+  } else {
+    message.reply("Commande inconnue.");
+  }
+});
+
+//on se connecte avec le token client
+client.login(token);

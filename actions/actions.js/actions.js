2d3e5c242ff369048610132136c24fdba7ea3a46
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-04-05 21:30:55 +0200

    add all actions and the action loader

---
+++ actions/actions.js/actions.js
@@ -0,0 +1,19 @@
+const actionSportives = require("./actions_sportives");
+const actionRelationelles = require("./actions_relationelles");
+const actionSanté = require("./actions_santé");
+const actionRepos = require("./actions_repos");
+const actionSociales = require("./actions_sociales");
+const actionIntellectuelles = require("./actions_intellectuelles");
+const actionDomestiques = require("./actions_domestiques");
+
+const allActions = [
+  ...actionSportives,
+  ...actionRelationelles,
+  ...actionSanté,
+  ...actionRepos,
+  ...actionSociales,
+  ...actionIntellectuelles,
+  ...actionDomestiques,
+];
+
+module.exports = allActions;

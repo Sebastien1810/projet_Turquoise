55a1b61377f7ae2f8b61ae5ee6c37b5f505378e1
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-04-05 12:51:56 +0200

    add the random stat for every perso in create personnage file

--- commands/createPersonnage.js
+++ commands/createPersonnage.js
@@ -1,6 +1,10 @@
 const createPersonnage = require("../commands/createPersonnage");
 const Personnage = require("../models/Personnage"); //j'importe mon modèle
 
+function randomStat(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 module.exports = {
   name: "createpersonnage",
   async execute(message, args) {
@@ -19,7 +23,16 @@ module.exports = {
         return message.reply("tu as déja un personnage !");
       }
       //crée un nouveau personnage avec les stats par défaut
-      const nouveauPerso = new Personnage({ userId, nom });
+      const nouveauPerso = new Personnage({
+        userId,
+        nom,
+        stats: {
+          bonheur: randomStat(30, 70),
+          energie: randomStat(30, 70),
+          sport: randomStat(5, 20),
+          intelligence: randomStat(5, 20),
+        },
+      });
       await nouveauPerso.save();
       message.reply(
         `Le nouveau personnage ${nom} a été invoqué et enregistré avec succès`
